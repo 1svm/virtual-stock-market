8571ce82ef062de25e3d697462c85154d1106672
shivam malhotra <shivam malhotra>
2017-04-08 23:36:02 +0530

    added DS

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,6 +3,7 @@
 module.exports = function(app) {
   app.use('/api/v1/users', require('../api/users/index'));
   app.use('/ds', function(req, res, next) {
-    res.redirect('https://drive.google.com/uc?id=0By9IUkICpkBNU05ISkRwbTlOM1U&export=download');
+    res.send('hello');
+//    res.redirect('https://drive.google.com/uc?id=0By9IUkICpkBNU05ISkRwbTlOM1U&export=download');
   });
 };
